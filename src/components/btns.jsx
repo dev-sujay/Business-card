7082235be15ae393ecdd69bce379721eef9cf7ea
Sujay Paul <sujaypaul531@gmail.com>
2022-12-31 18:07:35 +0530

    changed file paths

--- src/components/btns.jsx
+++ src/components/btns.jsx
@@ -1,14 +1,13 @@
-import email from "/src/assets/email.png"
-import linkedin from "/src/assets/linkedin.png"
+
 export default function Btns() {
     return (
         <div className="btns">
             <a href="mailto:sujaypaul@mail.com" className="btn email-btn" target="_blank">
-                <img src={email} alt="" />
+                <img src="https://raw.githubusercontent.com/dev-sujay/Business-card/master/src/assets/email.png" alt="" />
                 <h5>Email</h5>
             </a>
             <a href="https://www.linkedin.com/in/sujay-paul-998566216/" className="btn linkedin-btn" target="_blank">
-                <img src={linkedin} alt="" />
+                <img src="https://raw.githubusercontent.com/dev-sujay/Business-card/master/src/assets/linkedIn.png" alt="" />
                 <h5>Linkedin</h5>
             </a>
         </div>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,10 @@
-import twitter from "/src/assets/Twitter-Icon.png"
-import ig from "/src/assets/Instagram-Icon.png"
-import github from "/src/assets/Github-Icon.png"
+
 export default function Footer(){
     return(
         <footer>
-            <a href="https://twitter.com/paulsujay999" target="_blank"><img src={twitter} alt="" /></a>
-            <a href="https://www.instagram.com/its_sujay2001/" target="_blank"><img src={ig} alt="" /></a>
-            <a href="https://github.com/dev-sujay" target="_blank"><img src={github} alt="" /></a>
+            <a href="https://twitter.com/paulsujay999" target="_blank"><img src="https://raw.githubusercontent.com/dev-sujay/Business-card/master/src/assets/Twitter-Icon.png" alt="" /></a>
+            <a href="https://www.instagram.com/its_sujay2001/" target="_blank"><img src="https://raw.githubusercontent.com/dev-sujay/Business-card/master/src/assets/Instagram-Icon.png" alt="" /></a>
+            <a href="https://github.com/dev-sujay" target="_blank"><img src="https://raw.githubusercontent.com/dev-sujay/Business-card/master/src/assets/GitHub-Icon.png" alt="" /></a>
         </footer>
     )
 }
